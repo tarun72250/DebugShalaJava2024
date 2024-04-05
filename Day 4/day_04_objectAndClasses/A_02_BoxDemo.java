2b79d6d9d600a2cef9dc2d1441555a2d633cd0e5
tarun72250 <tarunrathore2904@gmail.com>
2024-04-05 13:16:01 +0530

    add msg

---
+++ Day 4/day_04_objectAndClasses/A_02_BoxDemo.java
@@ -0,0 +1,13 @@
+package day_04_objectAndClasses;
+
+public class A_02_BoxDemo {
+
+	public static void main(String[] args) {
+
+		A_02_Box b ;//refrence variable 
+		b = new A_02_Box();//allocating memory on heap
+		b.calVolume();//calling appropriate method
+		
+	}
+
+}

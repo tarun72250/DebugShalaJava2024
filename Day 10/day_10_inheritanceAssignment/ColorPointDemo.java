17b7a0d282a61c52a89c0f53d9ff0edf722bfa6e
tarun72250 <tarunrathore2904@gmail.com>
2024-04-16 13:21:17 +0530

    day 10 add

---
+++ Day 10/day_10_inheritanceAssignment/ColorPointDemo.java
@@ -0,0 +1,17 @@
+package day_10_inheritanceAssignment;
+
+public class ColorPointDemo {
+
+	public static void main(String[] args) {
+		
+		
+			ColorPoint cp1 = new ColorPoint(10, 20, "red");
+	        ColorPoint cp2 = new ColorPoint(30, 40, "green");
+	        ColorPoint cp3 = new ColorPoint(50, 60, "purple");
+
+	        System.out.println("ColorPoint 1: " + cp1.toString());
+	        System.out.println("ColorPoint 2: " + cp2);
+	        System.out.println("ColorPoint 3: " + cp3);
+	}
+
+}
